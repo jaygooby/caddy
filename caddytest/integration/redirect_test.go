c87cc330a3d1d4d44bc42be8919327aeca2d78aa
Jay Caines-Gooby <jay@gooby.org>
2023-01-10 20:00:14 +0000

    Test case 2 from https://github.com/caddyserver/caddy/issues/4205#issuecomment-863352037

--- caddytest/integration/redirect_test.go
+++ caddytest/integration/redirect_test.go
@@ -96,3 +96,82 @@ func TestArticleIndexRedirect(t *testing.T) {
 	// all articles should be served via /expert-caddy/template.html
 	tester.AssertGetResponse("http://localhost:9080/expert-caddy/foo", 200, "template body\n")
 }
+
+// Test case 2 from
+// https://github.com/caddyserver/caddy/issues/4205#issuecomment-863352037
+func TestNonCanonicalizedRewrittenFilename(t *testing.T) {
+
+	// temporary dir to hold the caddy config
+	tmpdir := t.TempDir()
+
+	// make the various nested dirs and their index.html
+	dirs := []string{"json", "modules"}
+	for _, dir := range dirs {
+		err := os.MkdirAll(filepath.Join(tmpdir,"docs",dir), os.ModePerm)
+		if err != nil {
+			t.Errorf("can't create dir: %s", err)
+	  }
+		// content is the same as the name of the file for testing purposes
+		err = os.WriteFile(filepath.Join(tmpdir,"docs",dir, "index.html"), []byte(dir), 0644)
+		if err != nil {
+			t.Errorf("can't write index file: %s", err)
+	  }
+	}
+
+	// make the /docs/index.html file
+	err := os.WriteFile(filepath.Join(tmpdir,"docs","index.html"), []byte("docs"), 0644)
+	if err != nil {
+		t.Errorf("can't write index file: %s", err)
+	}
+
+	// arrange
+	tester := caddytest.NewTester(t)
+	tester.InitServer(
+		fmt.Sprintf(`
+	  {
+	    admin localhost:2999
+	    http_port     9080
+	    https_port    9443
+	    grace_period  1ns
+	  }
+
+	  localhost:9080 {
+
+			 root * /tmp/caddy
+			 # %s
+
+			 file_server
+       templates
+       encode gzip
+
+       try_files {path}.html {path}
+
+       redir   /docs/json      /docs/json/
+       redir   /docs/modules   /docs/modules/
+       rewrite /docs/json/*    /docs/json/index.html
+       rewrite /docs/modules/* /docs/modules/index.html
+       rewrite /docs/*         /docs/index.html
+
+       reverse_proxy /api/* localhost:4444
+	  }
+	  `, tmpdir), "caddyfile")
+
+	// redir /docs/json
+	tester.AssertRedirect("http://localhost:9080/docs/json", "http://localhost:9080/docs/json/", 302)
+
+	// redir /docs/modules
+	tester.AssertRedirect("http://localhost:9080/docs/modules", "http://localhost:9080/docs/modules/", 302)
+
+	// index.html in /docs/json/ serves any file
+	tester.AssertGetResponse("http://localhost:9080/docs/json/foo", 200, "json")
+
+	// index.html in /docs/modules/ serves any file
+	tester.AssertGetResponse("http://localhost:9080/docs/modules/wat", 200, "modules")
+
+	// index.html in /docs/ serves any file except for json and modules
+	tester.AssertGetResponse("http://localhost:9080/docs/weevils", 200, "docs")
+
+	// index.html in /docs/ serves any file except for json and modules including
+	// nested paths
+	tester.AssertGetResponse("http://localhost:9080/docs/weevils/grub", 200, "docs")
+}
